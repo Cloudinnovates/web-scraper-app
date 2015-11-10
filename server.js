90e8c4cc41b9aacd3b071e3499f12dc2f2380204
IvanDobrovolsky <dobrovolsky94@gmail.com>
2015-11-10 22:13:08 +0200

    Created and run Node.js server.

---
+++ server.js
@@ -0,0 +1,41 @@
+//BASE SETUP
+"use strict";
+
+let config = require('./config'),
+    scrapper = require('./app/scrapper'),
+    express = require('express'),
+    morgan = require('morgan'),
+    fs = require('fs');
+
+
+
+//Initialize the app
+let app = express();
+
+
+//APP CONFIGURATION
+
+
+//Log all requests to the console
+app.use(morgan('dev'));
+
+//Set static files location
+//Used for requests that our front end will make
+app.use(express.static(__dirname  + '/public'));
+
+
+//Scrapper route
+app.get('/scrap', function (req, res) {
+   res.send(scrapper.scrapper);
+});
+
+//Main route
+//Send our users to front-end
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname + 'public/app/index.html'))
+});
+
+
+//START THE SERVER
+app.listen(config.port);
+console.log('The server is running on port ' + config.port);
