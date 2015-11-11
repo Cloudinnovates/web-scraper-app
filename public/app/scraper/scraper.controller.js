c79ee1f997956c4371a21cf6a577c3731944d7e7
IvanDobrovolsky <dobrovolsky94@gmail.com>
2015-11-12 00:38:08 +0200

    Implemented application UI with AngularMaterial framework. Created the form for inputting urls and added a validation.

--- public/app/scraper/scraper.controller.js
+++ public/app/scraper/scraper.controller.js
@@ -4,16 +4,33 @@
     angular.module('scraper')
         .controller("ScraperController", ScraperController);
 
-    console.log('scraper works...');
     function ScraperController(){
+
         let vm = this;
 
-        vm.message = "ScraperControllerWorks";
-
+        //Temporary helper variables
         vm.urlCount = 0;
+        vm.list = [];
 
+        //A helper function for iterating urls
         vm.range = function(){
             return new Array(vm.urlCount);
+        };
+
+        //All valid urls will be stored in this object
+        vm.urls = {};
+
+        //Convert urls from the temp list to the object
+        vm.convert = function () {
+            vm.list.forEach(function (url, index) {
+               vm.urls['url' + (index + 1)] = url;
+            });
+        };
+
+        //The function executes once the urlsForm is submitted
+        vm.start = function () {
+            vm.convert();
+            console.log(vm.urls + vm.urlCount);
         }
 
     }
