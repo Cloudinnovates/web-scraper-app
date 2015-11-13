13dd5abc9855d624677508d31fe6d2059884eb78
IvanDobrovolsky <dobrovolsky94@gmail.com>
2015-11-13 23:21:42 +0200

    Implemented Scraper functionality using es6 features.

--- app/scrapper.js
+++ app/scrapper.js
@@ -1,66 +1,42 @@
+//ECMAScript6 features work in strict mode
 "use strict";
-let xRay = require("x-ray");
 
-let xray = new xRay();
-
-let urls = [
-    "http://cad.edu.kpi.ua/petrenko/nove/",
-    "http://cad.edu.kpi.ua/petrenko/doslidnicka-robota/",
-"http://cad.edu.kpi.ua/petrenko/naukova-robota/",
-    "http://cad.edu.kpi.ua/petrenko/metodichna-robota-2/",
-        "http://cad.edu.kpi.ua/petrenko/premiї/",
-            "http://cad.edu.kpi.ua/rogoza/doslidnicka-robota/",
-                "http://cad.edu.kpi.ua/rogoza/metodichna-robota/",
-                    "http://cad.edu.kpi.ua/rogoza/insha-diyalnist/",
-                        "http://cad.edu.kpi.ua/rogoza/vixovna-robota/",
-                            "http://cad.edu.kpi.ua/britov/naukova-diyalnist/",
-                                "http://cad.edu.kpi.ua/stikanov/vidpochinok/",
-                                    "http://cad.edu.kpi.ua/stikanov/naukova-diyalnist/",
-                                        "http://cad.edu.kpi.ua/bulakh/biografiya/",
-                                            "http://cad.edu.kpi.ua/bulakh/metodichna-robota/",
-                                                "http://cad.edu.kpi.ua/bulakh/naukova-diyalnist/",
-                                                    "http://cad.edu.kpi.ua/bulakh/novini/",
-                                                        "http://cad.edu.kpi.ua/tsoorin/2012/03/21/diplomni-roboti/",
-                                                            "http://cad.edu.kpi.ua/tsoorin/2012/02/10/ekskurs-v-minule/",
-                                                                "http://cad.edu.kpi.ua/tsoorin/2011/06/16/vikonannya-dodatkovix-robit/",
-                                                                    "http://cad.edu.kpi.ua/tsoorin/2011/05/16/naukovi-interesi/",
-                                                                        "http://cad.edu.kpi.ua/tsoorin/2011/05/16/moї-predmeti/"
-];
+//Require x-ray module for scraping content
+let xRay = require("x-ray"),
+    xray = new xRay(); //Getting an instance of the module
 
 
+//Defining the class Scraper
+class Scraper{
 
-let options = {
-    title: 'title',
-    logo: '.bns-logo img@src, .post img@src',
-    content : ['.entry-content p, .entry p, #content p, .art-postcontent p']
-};
+    //The constructor which takes urls and options
+    constructor(urls, options){
+        this.urls = urls;
+        this.options =  options;
+    }
 
-function scrapeDataForOneUrl(url, options){
-    return new Promise(function (resolve, reject) {
-        xray(url, options)(function (err, doc) {
-            if(err){reject(err)}
-        })
-    });
-}
-function scrapeData(urls, options){
-    return new Promise(function (resolve, reject) {
-        let data = [];
+    //Implementing main method for scraping data
+    scrapeData(){
+        //Getting the reference at options property
+        var options = this.options;
 
-        for (let url of urls){
-            xray(url, options)(function (error, doc) {
-                if(error){
-                    reject('Something wend wrong!' + error);
-                }
-                data.push(doc);
-            })
+        //Function helper which takes a single url, makes a request and returns a promise
+        function scrapeDataForOneUr(url){
+            return new Promise( (resolve, reject) => {
+                xray(url, options)( (err, doc) => {
+
+                    if(err) reject(err);
+
+                    //Resolving the data object in case of success
+                    resolve(doc);
+                })
+            });
         }
-        resolve(data);
-    })
+
+        //The function returns a promise that resolves when all of the promises in the iterable
+        //argument have resolved or rejects with the reason of the first passed promise that rejects
+        return Promise.all(this.urls.map(scrapeDataForOneUr));
+    }
 }
 
-scrapeData(["http://google.com", "http://google.com"], 'title').then(function (data) {
-    console.log(data);
-}, function (error) {
-    console.log(error);
-});
-
+module.exports = Scraper;
