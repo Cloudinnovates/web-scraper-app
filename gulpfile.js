b33b07c1fb479c968c2a51dc5d421ea5a06cd900
IvanDobrovolsky <dobrovolsky94@gmail.com>
2015-11-30 03:39:33 +0200

    Fully implemented build automation with gulp.js(Injecting files, browser sync and serving back end)

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,103 @@
 "use strict";
-//NEEDED DEPENDENCIES
-const config = require('./config'),
+//<=====NEEDED DEPENDENCIES=====>
+const port = require('./config').port || process.env.PORT,
     gulp = require('gulp'),
     inject = require('gulp-inject'),
     nodemon = require('gulp-nodemon'),
-    browserSync = require('browser-sync').create(),
+    browserSync = require('browser-sync'),
     wiredep = require('wiredep').stream,
     chalk = require('chalk');
 
 
-//GULP CONFIGURATION
+//<=====GULP CONFIGURATION=====>
 //Directories and files
-const index = './public/index.html',
-    server = 'server.js',
-    client = './public',
-    port = config.port;
+const config = {
+    index: './public/index.html',
+    server: './server.js',
+    client: './public',
+    gulpfile: './gulpfile.js',
+    app: './app/',
+    all: '/**/*.*'
+};
 
-//Logging messages
+//Logger functions for messages and errors
 function log(message){
     console.log(chalk.white.bold.bgBlue(message));
 }
+function logError(message){
+    console.log(chalk.white.bold.bgRed(message));
+}
 
 
-//***********GULP TASKS**************
 
-//RESTART SERVER
-gulp.task("start-server", ['inject-bower'], () => {
-    let options = {
-        script: server,
-        delayTime: 1,
-        env: {
-            PORT: port,
-            'NODE_ENV': "development",
-            watch: server
-        }
-    };
+//*********GULP TASKS**************
 
-    log("***STARTING NODE SEVER***");
-    return nodemon(options);
-});
-
-//BROWSER SYNCHRONIZATION. RELOAD BROWSER.
-gulp.task("reload", () => {
-
-});
-
-
-//INJECT BOWER LIBRARIES TASK
+//<=====INJECTING BOWER LIBRARIES=====>
 gulp.task('inject-bower', () => {
     log("***INJECTING BOWER LIBRARIES***");
     let options = {};
     return gulp
-        .src(index)
+        .src(config.index)
         .pipe(wiredep(options))
-        .pipe(gulp.dest(client));
+        .pipe(gulp.dest(config.client))
+        .pipe(browserSync.stream());
 });
+
+
+//<===== RESTARTING SERVER =====>
+gulp.task("nodemon", (cb) => {
+
+    // We use this `started` variable to make sure the callback is only executed once
+    let started = false,
+
+        //Node server options
+        options = {
+        script: config.server,
+        env: {
+            PORT: port,
+            'NODE_ENV': "development",
+            //Watching backend files
+            watch: [config.server, config.gulpfile, config.app]
+        }
+    };
+
+    return nodemon(options)
+        .on('start', () => {
+            if(!started){
+                cb();
+                started = true;
+            }
+            log("***STARTING NODE SERVER***");
+        })
+        .on('restart', (files) => {
+            log("*** RESTARTING NODE SERVER ON FILES CHANGES: " + files + " ***");
+        })
+        .on('crash', () => {
+            logError("***NODE SERVER IS CRASHED***");
+        })
+        .on('exit', () => {
+            logError("***FINISHING NODE SERVER ***");
+        })
+});
+
+
+//<===== BROWSER SYNCHRONIZATION. RELOADING BROWSER =====>
+gulp.task("browser-sync", ['nodemon'], () => {
+
+    browserSync.init(null, {
+        // Tells BrowserSync on where the express app is running
+        proxy: "http://localhost:" + port,
+
+        // All of the following files will be watched - front end files
+        files: [config.client + config.all],
+
+        // This port should be different from the express app port
+        port: 7000
+    });
+
+    log("*** BROWSER SYNC IS WORKING ***");
+});
+
+
+//<===== DEFAULT TASK =====>
+gulp.task('default', ['browser-sync'], () => {});
